48c774828e4f6cb175c653c432abae886a80028f
Derrick Hammer <derrick@derrickhammer.com>
2022-09-20 15:05:16 -0400

    *Update exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { overwriteRegistryEntry } from "./registrywrite.js";
+export { readRegistryEntry } from "./registryread.js";
 export { generateSeedPhraseRandom } from "./seed.js";
 export { upload } from "./upload.js";
+export { progressiveFetch, progressiveFetchResult } from "./progressivefetch.js";
