5330d6019d918e0420b2f7293604b36db248e5ab
Derrick Hammer <derrick@derrickhammer.com>
2022-09-20 15:05:28 -0400

    *Update dist

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.upload = exports.generateSeedPhraseRandom = exports.overwriteRegistryEntry = void 0;
+exports.progressiveFetch = exports.upload = exports.generateSeedPhraseRandom = exports.readRegistryEntry = exports.overwriteRegistryEntry = void 0;
 var registrywrite_js_1 = require("./registrywrite.js");
 Object.defineProperty(exports, "overwriteRegistryEntry", { enumerable: true, get: function () { return registrywrite_js_1.overwriteRegistryEntry; } });
+var registryread_js_1 = require("./registryread.js");
+Object.defineProperty(exports, "readRegistryEntry", { enumerable: true, get: function () { return registryread_js_1.readRegistryEntry; } });
 var seed_js_1 = require("./seed.js");
 Object.defineProperty(exports, "generateSeedPhraseRandom", { enumerable: true, get: function () { return seed_js_1.generateSeedPhraseRandom; } });
 var upload_js_1 = require("./upload.js");
 Object.defineProperty(exports, "upload", { enumerable: true, get: function () { return upload_js_1.upload; } });
+var progressivefetch_js_1 = require("./progressivefetch.js");
+Object.defineProperty(exports, "progressiveFetch", { enumerable: true, get: function () { return progressivefetch_js_1.progressiveFetch; } });
